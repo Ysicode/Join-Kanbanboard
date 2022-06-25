8f71faa47b6442c8623b722dd2d7ec3ba6289715
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-06-25 10:40:27 +0100

    selection open Page navbar

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
-async function init (){
+async function init (page){
     await includeHTML();
+    navbarSelection(page);
+}
+
+function navbarSelection(page){
+    if(page != 'index'){
+        document.getElementById('navbarLinksSelection_' +page).style.backgroundColor = 'white' ;
+    }
 }
