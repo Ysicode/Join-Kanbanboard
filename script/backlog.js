551b9ef4cb838a5b4b4cc65a710da66090c92ece
Simon Weiss <simonweiss.ml@gmail.com>
2022-06-26 21:19:21 +0200

    added task overlay designs

---
+++ script/backlog.js
@@ -0,0 +1,7 @@
+function openSingleView() {
+    document.getElementById('task_overlay').classList.remove('d_none');
+}
+
+function closeSingleView() {
+    document.getElementById('task_overlay').classList.add('d_none');
+}
